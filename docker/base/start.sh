8772f614da1770fb486fd8bc3e65c3ece6abd604
roger <roger@aecerbot.ca>
2017-03-23 19:52:11 -0700

    fix groupid more

--- docker/base/start.sh
+++ docker/base/start.sh
@@ -7,22 +7,18 @@ set -e
 if [ $UID == 0 ]
 then
     # Change userid and possibly the groupid of NB_USER
-    if [ "$NOTEBOOK_UID" != $(id -u $NB_USER) ]
+    if [ "$NOTEBOOK_UID" != $(id -u $NB_USER) ] \
+        || [ "$NOTEBOOK_GID" != $(id -g $NB_USER) ]
     then
-        if [ "$NOTEBOOK_GID" != $(id -g $NB_USER) ]
-        then
-            groupadd -f -g $NOTEBOOK_GID $NB_GROUP
-            usermod -u $NOTEBOOK_UID -g $NOTEBOOK_GID $NB_USER
-        else
-            usermod -u $NOTEBOOK_UID $NB_USER
-        fi
-        chown $NB_USER:$NB_GROUP /$HOME/.jupyter/
+        groupmod -g $NOTEBOOK_GID $NB_GROUP
+        usermod -u $NOTEBOOK_UID -g $NOTEBOOK_GID $NB_USER
     fi
+    chown -R $NB_USER:$NB_GROUP /home/$NB_USER/.*
 
     # Exec the command as NB_USER
     echo "Running as UID:GID = $NOTEBOOK_UID:$NOTEBOOK_GID"
     exec su $NB_USER -c "env PATH=$PATH $*"
 else
     # Exec the command as a regular user
-    exec $*
+    exec jupyter notebook $*
 fi
